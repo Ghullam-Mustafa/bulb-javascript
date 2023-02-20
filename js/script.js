f18962be60e2b4a8db1b28fd607fdead4a61408e
Ghullam Mustafa <gmustafawaince180@gmail.com>
2023-02-20 21:42:18 +0500

    add border

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 function changestyle() {
     document.getElementById("first-paragraph").style.fontSize = "20px";
-        document.getElementById("first-paragraph").style.color = "green";
+    document.getElementById("first-paragraph").style.color = "green";
+    document.getElementById("first-paragraph").style.border = "2px solid blue";
 }
 
 changestyle();
